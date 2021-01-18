9298d688d0f5e0bb2d1378eab58cc85eb87f3591
David Beja <david.beja@gmail.com>
2021-01-18 13:00:11 +0000

    Fixes #41
    * Move settings to a dedicated tab. #41
    * String-related fixes. #41

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -4,58 +4,64 @@ class AffiliateWP_Checkout_Referrals_Admin {
 
 	public function __construct() {
 		// settings
-		add_filter( 'affwp_settings_integrations', array( $this, 'settings' ) );
+		add_filter( 'affwp_settings_tabs', array( $this, 'setting_tab' ) );
+		add_filter( 'affwp_settings',      array( $this, 'settings'    ) );
 	}
 
 	/**
-	 * Option to globally allow all affiliates to access order details
+	 * Register the new settings tab
 	 *
-	 * @since 1.0
-	 * @return boolean
+	 * @since 1.0.9
+	 * @return array
 	 */
-	public function settings( $fields ) {
+	public function setting_tab( $tabs ) {
+		$tabs['checkout_referrals'] = __( 'Checkout Referrals', 'affiliatewp-checkout-referrals' );
+		return $tabs;
+	}
 
-		$fields['checkout_referrals_header'] = array(
-			'name' => __( 'Checkout Referrals', 'affiliatewp-checkout-referrals' ),
-			'type' => 'header',
-		);
+	/**
+	 * Register the settings for Checkout Referrals tab
+	 *
+	 * @since 1.0.9
+	 * @return array
+	 */
+	public function settings( $settings ) {
 
-		$fields['checkout_referrals_checkout_text'] = array(
-			'name' => __( 'Checkout Text', 'affiliatewp-checkout-referrals' ),
-			'desc' => '<p class="description">' . __( 'Enter the field description to be displayed at checkout.', 'affiliatewp-checkout-referrals' ) . '</p>',
-			'type' => 'text',
-			'std'  => __( 'Who should be awarded commission for this purchase?', 'affiliatewp-checkout-referrals' )
-		);
-
-		$fields['checkout_referrals_affiliate_selection'] = array(
-			'name' => __( 'Affiliate Selection Method', 'affiliatewp-checkout-referrals' ),
-			'desc' => __( 'Choose how the customer will select the affiliate to award commission to.', 'affiliatewp-checkout-referrals' ),
-			'type' => 'select',
-			'options' => array(
-				'select_menu' => __( 'Customer selects affiliate from list', 'affiliatewp-checkout-referrals'),
-				'input'       => __( 'Customer enters affiliate\'s ID or username', 'affiliatewp-checkout-referrals'),
+		$settings['checkout_referrals'] = array(
+			'checkout_referrals_checkout_text' => array(
+				'name' => __( 'Checkout Text', 'affiliatewp-checkout-referrals' ),
+				'desc' => '<p class="description">' . __( 'Enter the field description to be displayed at checkout.', 'affiliatewp-checkout-referrals' ) . '</p>',
+				'type' => 'text',
+				'std'  => __( 'Who should be awarded commission for this purchase?', 'affiliatewp-checkout-referrals' )
+			),
+			'checkout_referrals_affiliate_selection' => array(
+				'name' => __( 'Affiliate Selection Method', 'affiliatewp-checkout-referrals' ),
+				'desc' => __( 'Choose how the customer will select the affiliate to award commission to.', 'affiliatewp-checkout-referrals' ),
+				'type' => 'select',
+				'options' => array(
+					'select_menu' => __( 'Customer selects affiliate from list', 'affiliatewp-checkout-referrals'),
+					'input'       => __( 'Customer enters affiliate&#8217;s ID or username', 'affiliatewp-checkout-referrals'),
+				)
+			),
+			'checkout_referrals_require_affiliate' => array(
+				'name' => __( 'Require Affiliate Selection', 'affiliatewp-checkout-referrals' ),
+				'desc' => __( 'Customer must select an affiliate to award the referral to.', 'affiliatewp-checkout-referrals' ),
+				'type' => 'checkbox'
+			),
+			'checkout_referrals_affiliate_display' => array(
+				'name' => __( 'Affiliate Display', 'affiliatewp-checkout-referrals' ),
+				'desc' => __( 'How the affiliates will be displayed at checkout.', 'affiliatewp-checkout-referrals' ),
+				'type' => 'radio',
+				'options' => array(
+					'user_nicename' => __( 'User Nicename', 'affiliatewp-checkout-referrals' ),
+					'display_name'  => __( 'Display Name', 'affiliatewp-checkout-referrals' ),
+					'nickname'      => __( 'Nickname', 'affiliatewp-checkout-referrals' )
+				),
+				'std' => 'user_nicename'
 			)
 		);
 
-		$fields['checkout_referrals_require_affiliate'] = array(
-			'name' => __( 'Require Affiliate Selection', 'affiliatewp-checkout-referrals' ),
-			'desc' => __( 'Customer must select an affiliate to award the referral to', 'affiliatewp-checkout-referrals' ),
-			'type' => 'checkbox'
-		);
-
-		$fields['checkout_referrals_affiliate_display'] = array(
-			'name' => __( 'Affiliate Display', 'affiliatewp-checkout-referrals' ),
-			'desc' => __( 'How the affiliates will be displayed at checkout', 'affiliatewp-checkout-referrals' ),
-			'type' => 'radio',
-			'options' => array(
-				'user_nicename' => 'User Nicename',
-				'display_name' => 'Display Name',
-				'nickname'     => 'Nickname'
-			),
-			'std' => 'user_nicename'
-		);
-
-		return $fields;
+		return $settings;
 	}
 
 }
